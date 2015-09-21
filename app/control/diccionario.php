5c7159b6891b282e5f0e3d2eebd9faf1f8ff2e70
sandyrod <sandyrod@gmail.com>
2015-09-21 16:13:37 -0430

    diseño de diccionario

---
+++ app/control/diccionario.php
@@ -0,0 +1,8 @@
+<?php
+
+class Diccionario extends Controller {
+	
+	 public function index() {
+		$this->view->render('diccionario/index');
+    }
+}
---
+++ app/view/diccionario/index.php
@@ -0,0 +1,34 @@
+<table class="tablas" width="370" border="1">
+	<tr>
+		<th colspan="2">Diccionario:</th>	
+	</tr>
+	
+	<tr>
+		<th>
+			Buscar:
+		</th>
+		<td>
+					<form>
+						<input type="text" name="palabra"> <input type="submit" name="ir" value="Buscar">				
+					</form>
+		</td>
+	</tr>
+	<tr>
+		<th>
+			Sinónimos:
+		</th>
+		<td>
+			<h2>palabra</h2><h2>similar</h2>
+		</td>
+	</tr>
+	<tr>
+		<th colspan="2">
+			Traducción:		
+		</th>	
+	</tr>
+	<tr>
+		<td  colspan="2">
+			aqui va la imagen resultante
+		</td>	
+	</tr>
+</table>
